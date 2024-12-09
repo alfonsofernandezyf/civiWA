addb4b2814e906261754cb1b5167c69d5cc5e8e1
Alfonso Fernández <alfonsofernandezyf@gmail.com>
2024-12-09 17:56:59 -0600

    Initial commit: CiviCRM WhatsApp Integration

---
+++ whatsapp.php
@@ -0,0 +1,24 @@
+<?php
+// WhatsApp integration functions
+
+function sendWhatsAppMessage($phone, $message) {
+    $url = "https://graph.facebook.com/v15.0/YOUR_PHONE_NUMBER_ID/messages";
+    $data = [
+        'messaging_product' => 'whatsapp',
+        'to' => $phone,
+        'type' => 'text',
+        'text' => ['body' => $message]
+    ];
+    $headers = [
+        'Authorization: Bearer YOUR_ACCESS_TOKEN',
+        'Content-Type: application/json'
+    ];
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
