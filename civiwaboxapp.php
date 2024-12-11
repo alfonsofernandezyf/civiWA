74cfb77c37fcb38ae16f0c86cfc605ec8c0ba068
Alfonso Fernández <alfonsofernandezyf@gmail.com>
2024-12-10 22:23:47 -0600

    debugging

--- civiwaboxapp.php
+++ civiwaboxapp.php
@@ -69,7 +69,11 @@ function civiwaboxapp_civicrm_pageRun($page) {
   }
 }
 
+/**
+ * Implements hook_civicrm_alterProviderTypes().
+ */
 function civiwaboxapp_civicrm_alterProviderTypes(&$providerTypes) {
   $providerTypes['waboxapp'] = 'CRM_Civiwaboxapp_Provider_WaboxAppProvider';
+  CRM_Core_Error::debug_log_message("WaboxApp registered as SMS provider.");
 }
 
